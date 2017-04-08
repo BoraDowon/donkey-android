5d820fba252a8cf98d5a0583413ca811a2ee58cf
Dowon Jeong <blessjdw@gmail.com>
2017-04-08 12:20:25 +0900

    improve ui for auth

--- donkey-android/src/main/java/donkey/bora/com/activity/EmailAuthActivity.java
+++ donkey-android/src/main/java/donkey/bora/com/activity/EmailAuthActivity.java
@@ -1,14 +1,22 @@
 package donkey.bora.com.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import donkey.bora.com.R;
 
 public class EmailAuthActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_code)
+    EditText authCodeEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,14 @@ public class EmailAuthActivity extends AppCompatActivity {
 
         setContentView(R.layout.email_auth_layout);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_send_email)
+    void onSendEmail(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        authCodeEdit.requestFocus();
     }
 
     @OnClick(R.id.confirm)

16ff8e5d6ab0ce299b850d0fa5bbbb2e32e6e6f2
Dowon Jeong <blessjdw@gmail.com>
2017-04-09 21:48:16 +0900

    added log for JsonResponseWrapper

--- donkey-android/src/main/java/donkey/bora/com/model/JsonResponseWrapper.java
+++ donkey-android/src/main/java/donkey/bora/com/model/JsonResponseWrapper.java
@@ -9,24 +9,26 @@ import retrofit2.Response;
 public abstract class JsonResponseWrapper<T> implements Callback<JsonResponse<T>> {
 
     private static final String TAG = "http";
+    private static final String OK_200 = "200";
 
     @Override
     public void onResponse(Call<JsonResponse<T>> call, Response<JsonResponse<T>> response) {
         if (response.body() != null) {
-            if ("200".equals(response.body().getCode())) {
-                Log.e(TAG, "[code]: " + response.body().getCode() + ", [msg]: " + response.body().getMsg() + ", [detail]: " + response.body().getDetail());
+            if (OK_200.equals(response.body().getCode())) {
+                Log.e(TAG, "onResponse: [code]: " + response.body().getCode() + ", [msg]: " + response.body().getMsg() + ", [detail]: " + response.body().getDetail());
                 callback(response.body().getData(), true);
                 return;
             }
-            Log.e(TAG, "[code]: " + response.body().getCode() + ", [msg]: " + response.body().getMsg() + ", [detail]: " + response.body().getDetail());
+            Log.e(TAG, "onResponse: [code]: " + response.body().getCode() + ", [msg]: " + response.body().getMsg() + ", [detail]: " + response.body().getDetail());
         } else {
-            Log.e(TAG, "[code]: " + response.raw().code() + ", [msg]: " + response.raw().message() + ", [detail]: " + response.body().getDetail());
+            Log.e(TAG, "onResponse: [code]: " + response.raw().code() + ", [msg]: " + response.raw().message() + ", [detail]: " + response.body().getDetail());
         }
         callback(null, false);
     }
 
     @Override
     public void onFailure(Call<JsonResponse<T>> call, Throwable t) {
+        Log.e(TAG, "onFailure: " + t.getMessage() + ", " + t.getCause());
         callback(null, false);
     }
 

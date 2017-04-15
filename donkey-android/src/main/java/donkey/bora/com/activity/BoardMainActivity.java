3b8126b6345384dea9f61e6f125b308ed24a5f36
Dowon Jeong <blessjdw@gmail.com>
2017-04-15 09:29:12 +0900

    added TableLayout on board activity

--- donkey-android/src/main/java/donkey/bora/com/activity/BoardMainActivity.java
+++ donkey-android/src/main/java/donkey/bora/com/activity/BoardMainActivity.java
@@ -2,17 +2,23 @@ package donkey.bora.com.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import donkey.bora.com.BackPressCloseHandler;
 import donkey.bora.com.R;
+import donkey.bora.com.model.BoardContentItemVO;
 import donkey.bora.com.model.InitResponseVO;
 
 public class BoardMainActivity extends AppCompatActivity {
 
     private BackPressCloseHandler backPressCloseHandler;
+
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +26,22 @@ public class BoardMainActivity extends AppCompatActivity {
 
         setContentView(R.layout.board_main_layout);
         ButterKnife.bind(this);
-
         backPressCloseHandler = new BackPressCloseHandler(this);
+
+        initTableLayout();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
+    private void initTableLayout() {
         Intent i = getIntent();
         InitResponseVO initResponseVO = (InitResponseVO) i.getSerializableExtra("init");
         if (initResponseVO != null) {
             String nickName = initResponseVO.getUser() != null ? initResponseVO.getUser().getNickname() : "guest";
             Toast.makeText(this, "환영합니다 " + nickName + "님", Toast.LENGTH_SHORT).show();
+
+            for (BoardContentItemVO itemVO: initResponseVO.getBoard()) {
+                tabLayout.addTab(tabLayout.newTab().setText(itemVO.getTitle()));
+            }
+            tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         }
     }
 
